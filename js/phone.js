2e8e5c4e929c6eb31151efee7d1b99128252ed50
sujonifty <sujonifty@gmail.com>
2024-02-28 09:47:17 +0600

    completed phone cards section

--- js/phone.js
+++ js/phone.js
@@ -1,26 +1,29 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText,isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    console.log(phones);
-    displayPhone(phones);
+    // console.log(phones);
+    displayPhone(phones, isShowAll);
  }
 //step-1: get the card container 
-const displayPhone = (phones) => {
+const displayPhone = (phones,isShowAll) => {
     // console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
   // show the button if phone length is greater than 12 
   const showAllBtn = document.getElementById('show-all-btn');
-  if (phones.length > 12){
+  if (phones.length > 12 && !isShowAll){
     showAllBtn.classList.remove('hidden');
   }
   else{
     showAllBtn.classList.add('hidden');
   }
-  //display 12 phones by slice
-  phones = phones.slice(0,12);
+  //display 12 phones by slice if not clicked show all button
+  if(!isShowAll){
+    phones = phones.slice(0,12);
 
+  }
+  
 
     phones.forEach(phone => {
         // console.log(phone);
@@ -36,7 +39,7 @@ const displayPhone = (phones) => {
         <h2 class="card-title">${phone.phone_name}</h2>
         <p>${phone.slug}</p>
         <div class="card-actions">
-          <button class="btn bg-rose-200">Buy Now</button>
+          <button onclick="handleShowDetails('${phone.slug}')" class="btn bg-rose-200">Show Details</button>
         </div>
       </div>`
       //step-4: set append child
@@ -45,13 +48,13 @@ const displayPhone = (phones) => {
     //remove loading spinner
     loadingSpinner(false);
 }
-const handleSearch = () =>{
+const handleSearch = (isShowAll) =>{
   loadingSpinner(true);
   const  searchField = document.getElementById('search-field');
 searchText = searchField.value;
-searchField.value ='';
+// searchField.value ='';
 // console.log(searchText);
-loadPhone(searchText);
+loadPhone(searchText, isShowAll);
 }
 
 //display loading spinner 
@@ -63,5 +66,17 @@ const loadingSpinner =(isSpinner) => {
   else{
     showSpinner.classList.add('hidden');
   }
+}
+
+//Show details of individual phone
+const handleShowDetails = async (id) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data = await res.json();
+  console.log(data);
+}
+
+// show all if clicked show all button. this is not proper way to show all
+const handleShowAll = () =>{
+  handleSearch(true);
 }
 // loadPhone();
