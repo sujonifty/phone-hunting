7ffcb5d9052879f0476d03eaa35a7e6714ea3f19
sujonifty <sujonifty@gmail.com>
2024-02-28 08:10:49 +0600

    show the all cards

--- js/phone.js
+++ js/phone.js
@@ -1,15 +1,29 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
+    console.log(phones);
     displayPhone(phones);
  }
 //step-1: get the card container 
 const displayPhone = (phones) => {
     // console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
+  // show the button if phone length is greater than 12 
+  const showAllBtn = document.getElementById('show-all-btn');
+  if (phones.length > 12){
+    showAllBtn.classList.remove('hidden');
+  }
+  else{
+    showAllBtn.classList.add('hidden');
+  }
+  //display 12 phones by slice
+  phones = phones.slice(0,12);
+
+
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         // step-2: create a div for phone card 
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card bg-base-100 shadow-xl`;
@@ -20,13 +34,34 @@ const displayPhone = (phones) => {
         </figure>
       <div class="card-body items-center text-center">
         <h2 class="card-title">${phone.phone_name}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <p>${phone.slug}</p>
         <div class="card-actions">
-          <button class="btn btn-primary">Buy Now</button>
+          <button class="btn bg-rose-200">Buy Now</button>
         </div>
       </div>`
       //step-4: set append child
       phoneContainer.appendChild(phoneCard);
     });
+    //remove loading spinner
+    loadingSpinner(false);
 }
-loadPhone();
+const handleSearch = () =>{
+  loadingSpinner(true);
+  const  searchField = document.getElementById('search-field');
+searchText = searchField.value;
+searchField.value ='';
+// console.log(searchText);
+loadPhone(searchText);
+}
+
+//display loading spinner 
+const loadingSpinner =(isSpinner) => {
+  const showSpinner = document.getElementById('loading-spinner');
+  if (isSpinner){
+    showSpinner.classList.remove('hidden');
+  }
+  else{
+    showSpinner.classList.add('hidden');
+  }
+}
+// loadPhone();
